8b20dc92edc71b66dbd27ed17601f1321ccb4897
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-04-30 05:43:54 +0000

    Validate and add book data to mylibraby on form submit
    - users data is saved accross page reloads in local storage

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const existingLibrary = localStorage.getItem("myLibrary")
-const myLibrary = existingLibrary != null ? existingLibrary : []
+// this logic handle data
 
+const existingLibrary = JSON.parse(localStorage.getItem("myLibrary"))
+let myLibrary = existingLibrary != null ? existingLibrary : []
+console.log(myLibrary);
 
-function Book(title, author, pages, isRead, rating = undefined, readExperience = undefined) {
+function Book(title, author, pages, isRead, rating, readExperience) {
     if (!new.target) {
         throw Error("new keyword must be used to Initialize book constructor")
     }
 
-    self.id = crypto.randomUUID()
-    self.title = title
-    self.author = author
-    self.pages = pages
-    self.isRead = isRead
-    self.rating = rating
-    self.readExperience = readExperience
-    self.img = "images/book.jpg"
+    this.id = crypto.randomUUID()
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+    this.rating = rating
+    this.readExperience = readExperience
+    this.img = "images/book.jpg"
 }
 
 
@@ -26,33 +28,69 @@ function addBookToLibrary(book) {
 
 function modifySavedArray() {
     try {
-        localStorage.setItem("myLibrary", myLibrary)
-    } 
+        localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
+    }
     catch (error) {
         if (error.name === 'QuotaExceededError' || error.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
             displayError("Storage is full. delete some books")
         }
-        else{
+        else {
             displayError("An error occur")
         }
     }
 }
 
+document.querySelector("#addBook").addEventListener("submit", (e) => {
+    hideError()
+    e.preventDefault()
+    const formData = new FormData(e.target);
 
-function displayError(message){
+    
+    const data = Object.fromEntries(formData);
+
+    const title = data["title"]
+    const author = data["author"]
+    const pages = +data["pages"]
+    const read = data["read"] == "on" ? true : false
+    const stars = data["rating"]
+    const readingExp = data["reading-experience"]
+
+    if (title && author && pages && pages != NaN){
+        const newBook = new Book(title, author, pages, read, readingExp)
+        addBookToLibrary(newBook)
+    }
+    else{
+        displayError("Fill Form Correctly")
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// these functions change ui
+
+function displayError(message) {
     const errorContainer = document.querySelector(".error")
     errorContainer.innerText = message
     errorContainer.style.height = "100px"
 }
 
+function hideError(){
+    const errorContainer = document.querySelector(".error")
+    errorContainer.style.height = "0px"
+}
 
-
-
-
-
-
-
-document.querySelector("#read").addEventListener("change",() => {
+// just disabled or enable reading experience section on the condition if user has check the read checkbox
+document.querySelector("#read").addEventListener("change", () => {
     const fieldSet = document.querySelector("fieldset")
     fieldSet.disabled = !fieldSet.disabled
 })
