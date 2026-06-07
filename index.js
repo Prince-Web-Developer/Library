13a3b12f552875c05abb1a9dce8048887c8ccc55
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-06-07 08:47:09 +0500

    Add custom error messages

--- index.js
+++ index.js
@@ -1,247 +1,225 @@
 // this logic handle data
 
-const existingLibrary = JSON.parse(localStorage.getItem("myLibrary"))
-let myLibrary = existingLibrary != null ? existingLibrary : []
-
+const existingLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+let myLibrary = existingLibrary != null ? existingLibrary : [];
 
 class Book {
-    constructor(title, author, pages, isRead, rating, readExperience) {
-        this.id = crypto.randomUUID()
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.isRead = isRead
-        this.rating = rating
-        this.readExperience = readExperience
-    }
+  constructor({
+    title,
+    author,
+    pages,
+    isRead,
+    rating = undefined,
+    readExperience = undefined,
+  }) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.rating = rating;
+    this.readExperience = readExperience;
+  }
 
-    addBookToLibrary() {
-        myLibrary.push(this)
-        this.modifySavedArray()
-    }
+  addBookToLibrary() {
+    myLibrary.push(this);
+    this.modifySavedArray();
+  }
 
-
-    modifySavedArray() {
-        try {
-            localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
-            changeDisplay("flex", "none")
-            displayBooks()
-        }
-        catch (error) {
-            if (error.name === 'QuotaExceededError' || error.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
-                displayError("Storage is full. delete some books")
-            }
-            else {
-                displayError("An error occur")
-            }
-        }
+  modifySavedArray() {
+    try {
+      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+      changeDisplay("flex", "none");
+      displayBooks();
+    } catch (error) {
+      if (
+        error.name === "QuotaExceededError" ||
+        error.name === "NS_ERROR_DOM_QUOTA_REACHED"
+      ) {
+        displayError("Storage is full. delete some books");
+      } else {
+        displayError("An error occur");
+      }
     }
+  }
 }
-
-
-
 
 document.querySelector("#addBook").addEventListener("submit", (e) => {
-    hideError()
-    e.preventDefault()
-    const formData = new FormData(e.target);
+  hideError();
+  e.preventDefault();
 
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
 
-    const data = Object.fromEntries(formData);
+  const isWeNeedToEditBook = document.querySelector("#exisitingValue").value;
 
-    const title = data["title"]
-    const author = data["author"]
-    const pages = +data["pages"]
-    const read = data["read"] == "on" ? true : false
-    let stars = undefined
-    let readingExp = undefined
-    if (read) {
-        stars = data["rating"]
-        readingExp = data["reading-experience"]
-    }
-
-    const isWeNeedToEditBook = data["exisitingValue"]
-
-
-    if (title && author && pages && pages != NaN) {
-
-        if (isWeNeedToEditBook || isWeNeedToEditBook != "") {
-            const index = FindElementWithId(isWeNeedToEditBook)
-            myLibrary.splice(index, 1)
-        }
-        const newBook = new Book(title, author, pages, read, stars, readingExp)
-        newBook.addBookToLibrary()
-        e.target.querySelector("fieldSet").disabled = true
-        e.target.reset()
-    }
-    else {
-        displayError("Fill Form Correctly")
-    }
-})
+  if (isWeNeedToEditBook || isWeNeedToEditBook != "") {
+    const index = FindElementWithId(isWeNeedToEditBook);
+    myLibrary.splice(index, 1);
+  }
+  const newBook = new Book(data);
+  newBook.addBookToLibrary();
+  e.target.querySelector("fieldSet").disabled = true;
+  e.target.reset();
+});
 
 function FindElementWithId(id) {
-    return myLibrary.findIndex(item => item.id == id)
+  return myLibrary.findIndex((item) => item.id == id);
 }
 
+const formField = document.querySelectorAll(".Rfield");
 
+formField.forEach((e) => {
+  e.addEventListener("input", () => checkFormField(e));
+  checkFormField(e);
+});
 
-
-
-
-
-
-
-
-
+function checkFormField(field) {
+  if (field.validity.valueMissing) {
+    field.setCustomValidity(`${field.name} is required`);
+    return;
+  }
+  field.setCustomValidity("");
+}
 
 // these functions change ui
 
 function displayError(message) {
-    const errorContainer = document.querySelector(".error")
-    errorContainer.innerText = message
-    errorContainer.style.height = "100px"
+  const errorContainer = document.querySelector(".error");
+  errorContainer.innerText = message;
+  errorContainer.style.height = "100px";
 }
 
 function hideError() {
-    const errorContainer = document.querySelector(".error")
-    errorContainer.style.height = "0px"
+  const errorContainer = document.querySelector(".error");
+  errorContainer.style.height = "0px";
 }
 
 // just disabled or enable reading experience section on the condition if user has check the read checkbox
 document.querySelector("#read").addEventListener("change", () => {
-    const fieldSet = document.querySelector("fieldset")
-    fieldSet.disabled = !fieldSet.disabled
-})
-
+  const fieldSet = document.querySelector("fieldset");
+  fieldSet.disabled = !fieldSet.disabled;
+});
 
 function changeDisplay(libraryDisplay, formDisplay) {
-    const library = document.querySelector(".library")
-    const form = document.querySelector(".form")
+  const library = document.querySelector(".library");
+  const form = document.querySelector(".form");
 
-
-    library.style.display = libraryDisplay
-    form.style.display = formDisplay
+  library.style.display = libraryDisplay;
+  form.style.display = formDisplay;
 }
 
-
 function makeFormHavePreviousValue(e) {
-    const bookId = e.target.id
-    const element = myLibrary[FindElementWithId(bookId)]
-    if (element) {
-        fillForm(element)
-        changeDisplay("none", "flex")
-    }
-    else {
-        displayError("Item Not Found")
-    }
-
+  const bookId = e.target.id;
+  const element = myLibrary[FindElementWithId(bookId)];
+  if (element) {
+    fillForm(element);
+    changeDisplay("none", "flex");
+  } else {
+    displayError("Item Not Found");
+  }
 }
 
 function fillForm(ele) {
-    const form = document.querySelector("form")
+  const form = document.querySelector("form");
 
-    const titleInputField = form.querySelector("#title")
-    titleInputField.value = ele.title
+  const titleInputField = form.querySelector("#title");
+  titleInputField.value = ele.title;
 
-    const authorInputField = form.querySelector("#author")
-    authorInputField.value = ele.author
+  const authorInputField = form.querySelector("#author");
+  authorInputField.value = ele.author;
 
-    const pagesInputField = form.querySelector("#pages")
-    pagesInputField.value = ele.pages
+  const pagesInputField = form.querySelector("#pages");
+  pagesInputField.value = ele.pages;
 
-    const isRead = ele.isRead
-    const readCheckBox = form.querySelector("#read")
-    readCheckBox.checked = isRead
+  const isRead = ele.isRead;
+  const readCheckBox = form.querySelector("#read");
+  readCheckBox.checked = isRead;
 
+  const fieldSet = form.querySelector("fieldset");
+  fieldSet.disabled = !isRead;
 
-    const fieldSet = form.querySelector("fieldset")
-    fieldSet.disabled = !isRead
+  const rateInputField = form.querySelector("#rate");
+  const reviewInputField = form.querySelector("#desc");
 
-    const rateInputField = form.querySelector("#rate")
-    const reviewInputField = form.querySelector("#desc")
+  const rating = ele.rating;
+  if (rating) {
+    rateInputField.value = rating;
+  }
 
-    const rating = ele.rating
-    if (rating) {
-        rateInputField.value = rating
-    }
+  const review = ele.readExperience;
+  if (review) {
+    reviewInputField.value = review;
+  }
 
-    const review = ele.readExperience
-    if (review) {
-        reviewInputField.value = review
-    }
-
-    const bookId = form.querySelector("#exisitingValue")
-    bookId.value = ele.id
+  const bookId = form.querySelector("#exisitingValue");
+  bookId.value = ele.id;
 }
 
 function displayBooks() {
-    const main = document.querySelector("main")
-    if (myLibrary.length == 0) {
-        const div = document.createElement("div")
-        div.innerText = "No Books Added"
-        div.classList.add("centerText")
-        main.appendChild(div)
-        return null
+  const main = document.querySelector("main");
+  if (myLibrary.length == 0) {
+    const div = document.createElement("div");
+    div.innerText = "No Books Added";
+    div.classList.add("centerText");
+    main.appendChild(div);
+    return null;
+  }
+  const template = cleanMain();
+  main.appendChild(template);
+  myLibrary.forEach((e, index) => {
+    const templateClone = template.cloneNode(true);
+    templateClone.style.display = "flex";
+
+    const editButton = templateClone.querySelector(".editbtn");
+    editButton.id = e.id;
+
+    const readIcon = templateClone.querySelector(".icon");
+    const titleContainer = templateClone.firstElementChild;
+    const starContainer = templateClone.querySelector(".idk");
+    const pageContainer = templateClone.querySelector(".pagecount");
+    const readingExperienceContainer = templateClone.querySelector(
+      ".reading-experience",
+    );
+    const authorContainer = templateClone.querySelector(".by");
+
+    titleContainer.innerText = e.title;
+    pageContainer.innerText = `${e.pages} pages`;
+    authorContainer.innerText = `by ${e.author}`;
+
+    if (e.rating) {
+      starContainer.textContent = e.rating;
+    } else {
+      starContainer.innerText = "N/A";
     }
-    const template = cleanMain()
-    main.appendChild(template)
-    myLibrary.forEach((e, index) => {
-        const templateClone = template.cloneNode(true)
-        templateClone.style.display = "flex"
 
-        const editButton = templateClone.querySelector(".editbtn")
-        editButton.id = e.id
+    if (e.readExperience) {
+      readingExperienceContainer.innerText = e.readExperience;
+    } else if (!e.isRead) {
+      readingExperienceContainer.innerText = "Not read yet";
+    } else {
+      readingExperienceContainer.innerText = "N/A";
+      readingExperienceContainer.classList.add("grey");
+    }
 
-        const readIcon = templateClone.querySelector(".icon")
-        const titleContainer = templateClone.firstElementChild
-        const starContainer = templateClone.querySelector(".idk")
-        const pageContainer = templateClone.querySelector(".pagecount")
-        const readingExperienceContainer = templateClone.querySelector(".reading-experience")
-        const authorContainer = templateClone.querySelector(".by")
-
-
-        titleContainer.innerText = e.title
-        pageContainer.innerText = `${e.pages} pages`
-        authorContainer.innerText = `by ${e.author}`
-
-        if (e.rating) {
-            starContainer.textContent = e.rating
-        }
-        else {
-            starContainer.innerText = 'N/A'
-        }
-
-        if (e.readExperience) {
-            readingExperienceContainer.innerText = e.readExperience
-        }
-        else if (!e.isRead) {
-            readingExperienceContainer.innerText = "Not read yet"
-        }
-        else {
-            readingExperienceContainer.innerText = "N/A"
-            readingExperienceContainer.classList.add("grey")
-        }
-
-        if (e.isRead) {
-            readIcon.classList.add("icon-tick")
-        }
-        else {
-            readIcon.classList.add("icon-cross")
-        }
-        main.appendChild(templateClone)
-    });
+    if (e.isRead) {
+      readIcon.classList.add("icon-tick");
+    } else {
+      readIcon.classList.add("icon-cross");
+    }
+    main.appendChild(templateClone);
+  });
 }
 
 function cleanMain() {
-    const main = document.querySelector("main")
-    const template = main.firstElementChild
-    main.textContent = ""
-    return template
+  const main = document.querySelector("main");
+  const template = main.firstElementChild;
+  main.textContent = "";
+  return template;
 }
 
-
 document.querySelector("#add-button-on-main").addEventListener("click", () => {
-    changeDisplay("none", "flex")
-})
+  changeDisplay("none", "flex");
+});
 
-window.displayBooks()
+window.displayBooks();
